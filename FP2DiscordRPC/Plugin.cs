74dddaec4c63514ea6918fd0c6ebb81d5456ed1a
techmuse <44119098+techmuse8@users.noreply.github.com>
2025-07-19 12:08:27 -0400

    load the discord game sdk dll from the mod folder on game boot
    This helps to make the installation process more user friendly

--- FP2DiscordRPC/Plugin.cs
+++ FP2DiscordRPC/Plugin.cs
@@ -2,6 +2,8 @@
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,10 +18,33 @@ namespace DiscordFP2
         public static FP2Discord gameDiscord;
         public static ManualLogSource consoleLog;
 
+        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern IntPtr LoadLibrary(string lpFileName);
+
         private void Awake()
         {
             consoleLog = Logger;
             consoleLog.LogInfo($"Plugin FP2DiscordRPC loading...");
+
+
+            // Before we do anything, we need to load the Discord Game SDK DLL
+            string pluginDir = Path.GetDirectoryName(Info.Location);
+            string dllPath = Path.Combine(pluginDir, "discord_game_sdk.dll");
+
+            if (!File.Exists(dllPath))
+            {
+                consoleLog.LogError($"discord_game_sdk.dll not found at: {dllPath}!");
+                return;
+            }
+
+            IntPtr handle = LoadLibrary(dllPath);
+            if (handle == IntPtr.Zero)
+            {
+                int err = Marshal.GetLastWin32Error();
+                consoleLog.LogError($"Failed to load discord_game_sdk.dll! Win32 Error Code: {err}");
+                return;
+            }
+
             gameDiscord = new FP2Discord();
             consoleLog.LogInfo($"Plugin FP2DiscordRPC 1.0.0 is now running!");
         }
